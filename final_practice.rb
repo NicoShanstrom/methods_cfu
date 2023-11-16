5fc6aacb52005c0c40c234f5997dcc1582a64a4d
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-11-16 13:02:50 -0500

    Add my name to argument

--- final_practice.rb
+++ final_practice.rb
@@ -37,7 +37,7 @@ def greet_person(first, middle, last)
     "Hello, #{first} #{middle} #{last}"
 end
 
-p greet_person
+p greet_person ("Nicholas", "Robert", "Shanstrom")
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
